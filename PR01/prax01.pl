f74f4e107ef96ab4dfe51b60c13218d785d564bd
Kaspar Ustav <kaspar.ustav@gmail.com>
2021-09-07 16:43:41 +0300

    vend

--- PR01/prax01.pl
+++ PR01/prax01.pl
@@ -15,3 +15,4 @@ female(liis).
 female(evelin).
 
 father(Child, Father):- mother(Child,Mother), married(Mother, Father), male(Father).
+brother(Child, Brother):- male(Brother), ((mother(Child, Mother), mother(Brother, Mother));(father(Child, Father), father(Brother, Father))), not(Child=Brother).
