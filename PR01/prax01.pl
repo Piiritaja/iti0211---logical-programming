90a3f219567932e07888bbac0c05f8610e4db844
Kaspar Ustav <kaspar.ustav@gmail.com>
2021-09-07 16:17:04 +0300

    testime

--- PR01/prax01.pl
+++ PR01/prax01.pl
@@ -1,1 +1,14 @@
+mother(liis, maris).
+mother(ago, liis).
+
+married(evelin, ago).
+married(liis, mikk).
+
 male(sedrik).
+male(ago).
+male(kaspar).
+male(mikk).
+
+female(maris).
+female(liis).
+female(evelin).
