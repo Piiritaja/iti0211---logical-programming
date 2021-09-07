1af2e2935726d1a3e24b98548e763e65ebd873cb
Kaspar Ustav <kaspar.ustav@gmail.com>
2021-09-07 16:57:37 +0300

    fix

--- PR01/prax01.pl
+++ PR01/prax01.pl
@@ -17,6 +17,6 @@ female(evelin).
 father(Child, Father):- mother(Child,Mother), married(Mother, Father), male(Father).
 brother(Child, Brother):- male(Brother), ((mother(Child, Mother), mother(Brother, Mother));(father(Child, Father), father(Brother, Father))), not(Child=Brother).
 sister(Child, Sister):- female(Sister), ((mother(Child, Mother), mother(Sister, Mother));(father(Child, Father), father(Sister, Father))), not(Sister=Child).
-parent(Child, Parent):- mother(Child, Paretn); father(Child, Parent).
+parent(Child, Parent):- mother(Child, Parent); father(Child, Parent).
 aunt(Child, Aunt):- parent(Child, Parent), sister(Parent, Aunt).
 uncle(Child, Uncle):- parent(Child, Parent), brother(Parent, Uncle).
