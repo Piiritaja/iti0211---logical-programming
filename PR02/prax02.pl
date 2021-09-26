9213f6757d780d887bbb86abd5f5d714711f6291
Piiritaja <54994820+Piiritaja@users.noreply.github.com>
2021-09-27 01:14:47 +0300

    req 6

--- PR02/prax02.pl
+++ PR02/prax02.pl
@@ -44,6 +44,7 @@ uncle(Child, Uncle):- parent(Child, Parent), brother(Parent, Uncle).
 grandfather(Child, Grandfather):- (father(Child, Father), father(Father, Grandfather));(mother(Child, Mother), father(Mother, Grandfather)).
 grandmother(Child, Grandmother):- mother(Child, Mother), mother(Mother, Grandmother);(father(Child, Father), mother(Father, Grandmother)).
 
+%2. Lisa teadmusbaasile rekursiivne reegel ancestor/2, mis leiab isiku kõik esivanemad.
 ancestor(Child, Parent) :- 
     parent(Child, Parent). 
 
@@ -51,12 +52,27 @@ ancestor(Child, Ancestor) :-
     parent(Child, Parent),
     ancestor(Parent, Ancestor). 
 
+%3. Lisa teadmusbaasile rekursiivne reegel male_ancestor/2, mis leiab meessoost esivanemad.
 male_ancestor(Child, Parent):- ancestor(Child, Parent), male(Parent).
+
+%4. Lisa teadmusbaasile rekursiivne reegel female_ancestor/2, mis leiab naissoost esivanemad. 
 female_ancestor(Child, Parent):- ancestor(Child, Parent), female(Parent).
 
+%5. Lisa teadmusbaasile reegel ancestor1/3, mis leiab N-nda sugupõlve esivanemad (nii mees- kui ka naissoost). 
 ancestor1(Child, Ancestor, N) :- N == 1, parent(Child, Ancestor).
 
 ancestor1(Child, Ancestor, N) :-
     X is -(N, 1),
     parent(Child, Parent),
     ancestor1(Parent, Ancestor, X).
+
+
+%6. Lisa teadmusbaasile reegel ancestor2/3, mis leiab esivanemad, kellel on rohkem kui X last.
+ancestor2(Child, Parent, X):-
+    bagof(Child, parent(Child, Parent), Bag),
+    length(Bag, Lenght),
+    Lenght > X.
+
+ancestor2(Child, Ancestor, X):- 
+    parent(Child, Parent),
+    ancestor2(Parent, Ancestor, X).
