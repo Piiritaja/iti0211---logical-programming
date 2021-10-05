343a1046804de637aaa4f3cd2b55eb17f9d05f83
Piiritaja <54994820+Piiritaja@users.noreply.github.com>
2021-10-05 16:59:24 +0300

    kolmas

--- PR04/prax04.pl
+++ PR04/prax04.pl
@@ -6,9 +6,20 @@ lennukiga(tallinn, helsinki, 30).
 lennukiga(helsinki, paris, 180).
 lennukiga(paris, berlin, 120).
 lennukiga(paris, tallinn, 120).
+lennukiga(stockholm,paris,120).
 
 
-tee(X,Y):- laevaga(X,Y,_); bussiga(X,Y,_); rongiga(X,Y,_); lennukiga(X,Y,_).
+tee(X,Y):- 
+    laevaga(X,Y,_);
+    bussiga(X,Y,_);
+    rongiga(X,Y,_);
+    lennukiga(X,Y,_).
 %2. Lisa teadmusbaasile rekursiivne reegel reisi/2, mis leiab, kas on võimalik reisida ühest linnast teise. Ühenduse leidmisel võib olla vajalik kombineerida omavahel erinevaid transpordi vahendeid. 
-reisi(X,Y):- tee(X,Y).
+reisi(X,Y):- tee(X,Y), !.
 reisi(X,Z):- tee(X,Y), reisi(Y,Z).
+
+%3. Lisa teadmusbaasile reegel reisi/3, mis lisaks eelnevale näitab ka teel läbitavad linnad.
+reisi(X,Y,mine(X,Y)):- tee(X,Y), !.
+reisi(X,Z, mine(X,Y,Tee)):-
+    tee(X,Y),
+    reisi(Y,Z,Tee).
