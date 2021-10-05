8d00ccab1cc66228e0312e274b769dd9ba7dddd3
Piiritaja <54994820+Piiritaja@users.noreply.github.com>
2021-10-05 17:41:26 +0300

    viimane

--- PR04/prax04.pl
+++ PR04/prax04.pl
@@ -8,16 +8,16 @@ lennukiga(paris, berlin, 120).
 lennukiga(paris, tallinn, 120).
 lennukiga(stockholm,paris,120).
 
-tee(X, Y, lennukiga) :- lennukiga(X, Y, _).
-tee(X, Y, rongiga) :- rongiga(X, Y, _).
-tee(X, Y, bussiga) :- bussiga(X, Y, _).
-tee(X, Y, laevaga) :- laevaga(X, Y, _).
+tee(X, Y, Cost, lennukiga) :- lennukiga(X, Y, Cost).
+tee(X, Y, Cost, rongiga) :- rongiga(X, Y, Cost).
+tee(X, Y, Cost, bussiga) :- bussiga(X, Y, Cost).
+tee(X, Y, Cost, laevaga) :- laevaga(X, Y, Cost).
 
-tee(X,Y):- 
-    laevaga(X,Y,_);
-    bussiga(X,Y,_);
-    rongiga(X,Y,_);
-    lennukiga(X,Y,_).
+tee(X,Y,Cost):- 
+    laevaga(X,Y,Cost);
+    bussiga(X,Y,Cost);
+    rongiga(X,Y,Cost);
+    lennukiga(X,Y,Cost).
 %2. Lisa teadmusbaasile rekursiivne reegel reisi/2, mis leiab, kas on võimalik reisida ühest linnast teise. Ühenduse leidmisel võib olla vajalik kombineerida omavahel erinevaid transpordi vahendeid. 
 reisi(X,Y):- tee(X,Y), !.
 reisi(X,Z):- tee(X,Y), reisi(Y,Z).
@@ -29,8 +29,14 @@ reisi(X,Z, mine(X,Y,Tee)):-
     reisi(Y,Z,Tee).
 
 %4. Lisa teadmusbaasile reegel reisi_transpordiga/3, mis lisaks eelnevale näitab ka seda, millise transpordivahendiga antud vahemaa läbitakse.
-reisi_transpordiga(X,Y,mine(X,Y,Transport)):- tee(X,Y, Transport), !.
+reisi_transpordiga(X,Y,mine(X,Y,Transport)):- tee(X,Y, _,Transport), !.
 reisi_transpordiga(X,Z, mine(X,Y,Transport,Tee)):-
-    tee(X,Y,Transport),
-    tee(X,Y),
+    tee(X,Y,_,Transport),
     reisi_transpordiga(Y,Z,Tee).
+
+%5. Lisa teadmusbaasile reegel reisi/4, mis näitab läbitavaid linnu, millise transpordivahendiga antud vahemaa läbitakse ja reisiks kuluvat aega alguspunktist lõpppunkti.
+reisi(X,Y,mine(X,Y,Transport),Hind):- tee(X,Y, Hind, Transport). 
+reisi(X,Z,mine(X,Y,Transport,Tee), Hind):-
+    tee(X,Y,Cost,Transport),
+    reisi(Y,Z,Tee,Price),
+    Hind is +(Cost,Price)
