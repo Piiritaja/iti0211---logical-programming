c92c64502b0b252058814310feeab6da3c44e664
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2021-11-01 23:14:34 +0200

    added airports

--- PR06/main.py
+++ PR06/main.py
@@ -1,3 +1,4 @@
+from typing import List
 import requests
 from creds import token
 import time
@@ -6,19 +7,19 @@ from pyswip import Prolog
 "https://api.lufthansa.com/v1/flight-schedules/flightschedules/passenger?airlines=LH&startDate=25OCT21&endDate=25OCT21&daysOfOperation=1234567&timeMode=UTC" -H  "accept: application/json" -H  "Authorization: Bearer r467s5wz9r5525n8bp5ap99d"
 """
 airlines = "LH"
-startDate = "25OCT21"
-endDate = "25OCT21"
+startDate = "01NOV21"
+endDate = "01NOV21"
 #r = requests.get(f'https://api.lufthansa.com/v1/flight-schedules/flightschedules/passanger?airlines={airlines}&startDate={startDate}&endDate={endDate}&daysOfOperation=1234567&timeMode=UTC')
 payload = {'directFlights': 0}
 headers = {"Authorization": 'Bearer ' + token, "Accept": "application/json", "X-Originating-IP": "80.235.76.174"}
-date = "2021-10-26"
+date = "2021-11-02"
 
 def get_query(schedule):
     try:
         flights = schedule["Flight"]
     except TypeError:
         return -1
-    querys = []
+    querys = set()
     if isinstance(flights,list):
         for flight in flights:
             cost = flight["MarketingCarrier"]["FlightNumber"]
@@ -26,14 +27,14 @@ def get_query(schedule):
 
             arrival = get_flight_data(flight["Arrival"])
             query = f"lennukiga{departure[0], arrival[0], cost, departure[1], arrival[1]}."
-            querys.append(query)
+            querys.add(query)
     else:
         cost = flights["MarketingCarrier"]["FlightNumber"]
         departure = get_flight_data(flights["Departure"])
         arrival = get_flight_data(flights["Arrival"])
         query = f"lennukiga{departure[0], arrival[0], cost, departure[1], arrival[1]}."
-        querys.append(query)
-    return querys
+        querys.add(query)
+    return list(querys)
 
 def get_flight_data(data):
     x = data["AirportCode"].lower()
@@ -42,9 +43,11 @@ def get_flight_data(data):
     return x, time
 
 def get_data():
-    codes = ["TLL", "CPH", "FRA", "HEL", "MAN", "MUC"]
-    data = []
+    codes = ["TLL", "CPH", "FRA", "HEL", "BRE", "CTA", "IVL", "HRK", "VNO", "KBP", "FCO", "BER", "BUE", "CHI", "LON", "MIL", "MOW", "NYC", "PAR", "ROM", "SAO", "STO", "TYO", "WAS", "LCG", "NCY", "ASE", "BIQ", "BIO", "BON", "CHQ", "CJS", "DUR"]
+    data = set()
     for c1 in codes:
+        if len(data) > 500:
+            break
         for c2 in codes:
             if c1 == c2:
                 continue
@@ -54,10 +57,15 @@ def get_data():
             if r.status_code != 200:
                 continue
             schedules = r.json()["ScheduleResource"]["Schedule"]
-            for schedule in schedules:
-                querys = get_query(schedule)
-                if querys != -1:
-                    data.extend(querys)
+            if isinstance(schedules, List):
+                for schedule in schedules:
+                    querys = get_query(schedule)
+                    if querys != -1:
+                        for q in querys:
+                            data.add(q)
+            else:
+                for q in querys:
+                    data.add(q)
     return data
 
 def load_data_to_file(data, file):
@@ -77,8 +85,11 @@ def do_query():
     prolog = Prolog()
     prolog.consult("prax06.pl")
     for query in data:
-        prolog.asserta(query)
-    for sol in prolog.query("reisi(tll, Y)"):
+        prolog.assertz(query)
+    count = 0
+    for sol in prolog.query("reisi(tll, fco, Tee, Hind, Aeg)."):
+        count += 1
+        print(f"{count} paths found.")
         print(sol)
     return load_data_from_file("data.pl")
 
