03339b3489b41d7c2492ff263f366d747cb58229
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2021-11-22 20:17:11 +0200

    praks 9

---
+++ PR09/prax09.pl
@@ -0,0 +1,33 @@
+/*
+Ülesanne 3: Koostada järgmiste eesti keele lausete parsimiseks ühine grammatika puu, mis kirjeldab kõigi kolme antud lausete süntaksi: 
+
+    veerevale kivile sammal ei kasva (juursümbol: 'lihtlause') 
+    uhkus ajab upakile (juursümbol: 'lihtlause')
+    raha tuleb, raha laheb, volad jaavad (juursümbol: 'liitlause') 
+
+▪ NB! komad on ka lause osad 
+▪ Reegel 'liitlause' peab olema rekursiivne e. tunnistama peab ka lause “raha tuleb, raha tuleb, raha laheb, volad jaavad”
+*/
+
+lihtlause --> nimisonafraas, tegusonafraas.
+lihtlause --> nimisona, tegusonafraas.
+nimisonafraas --> maarsonafraas, nimisona.
+nimisonafraas --> maarsona, nimisona.
+maarsonafraas --> omadussona, maarsona.
+tegusonafraas --> [ei], tegusona, maarsonafraas.
+tegusonafraas --> tegusona, maarsonafraas.
+liitlause --> lihtlause, [,], lihtlause.
+liitlause --> lihtlause, [,], liitlause.
+
+nimisona --> [sammal] ; [uhkus] ; [raha] ; [volad].
+maarsona --> [kivile] ; [upakile].
+omadussona --> [veerevale].
+tegusona --> [kasva] ; [ajab] ; [tuleb] ; [laheb] ; [jaavad].
+
+/*
+veerevale kivile sammal ei kasva (juursümbol: 'lihtlause') 
+phrase(lihtlause, [veerevale, kivile, sammal, ei, kasva]).
+phrase(lihtlause, [uhkus, ajab, upakile]).
+phrase(liitlause, [raha, tuleb, ',', raha, laheb, ',', volad, jaavad]).
+
+*/
