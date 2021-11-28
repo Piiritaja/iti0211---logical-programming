4563d45daaebced5f81773e18a33aa9b523d1ca2
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2021-11-28 22:46:08 +0200

    praks 10

--- PR10/prax10.pl
+++ PR10/prax10.pl
@@ -176,8 +176,11 @@ status:-
 
 vota(X,Y,Suund,X1,Y1,X2,Y2) :- 
     retract(ruut(X1, Y1, _),
+    asserta(ruut(X1, Y1, 0)),
     tee_kaik(X, Y, X2, Y2)).
 
 tee_kaik(X,Y,X1,Y1) :- 
     retract(ruut(X, Y, C)),
+    retract(ruut(X1, Y1, _)),
+    asserta(ruut(X, Y, 0)),
     asserta(ruut(X1, Y1, C)).
